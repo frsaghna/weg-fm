a11d581da53469703fb2263ad43b8a74464add91
Faris Aghna <frsaghna@gmail.com>
2026-07-26 20:37:37 +0700

    rel: Release v1.0.0 featuring full Phase 0-9 feature suite

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="weg-fm",
-    version="0.1.0",
+    version="1.0.0",
     description="Keyboard-first GTK4 file manager for Linux",
     author="weg-fm contributors",
     license="GPL-3.0-or-later",
