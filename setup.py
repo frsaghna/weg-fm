c851dbe177a4f2e6c1160b074e83d7f31bb4e9c2
Faris Aghna <frsaghna@gmail.com>
2026-07-26 21:48:25 +0700

    rel: Release v1.0.4 with clean source extraction in PKGBUILD

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="weg-fm",
-    version="1.0.2",
+    version="1.0.4",
     description="Keyboard-first GTK4 file manager for Linux",
     author="weg-fm contributors",
     license="GPL-3.0-or-later",
